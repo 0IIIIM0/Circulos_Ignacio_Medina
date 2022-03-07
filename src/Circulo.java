3f7ce6addc6903a43bd1504c94428a2e9afa5bd4
0IIIIM0 <ignac23med@gmail.com>
2022-03-07 11:49:43 -0600

    Creation of the class "Main"

--- src/Circulo.java
+++ src/Circulo.java
@@ -1,2 +1,50 @@
 public class Circulo {
+    //atributo radio privado
+   private double radio;
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public Circulo (){
+        System.out.println("No sirvo para nada");
+    }
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    // metodos
+
+    public double calculararea (){
+        double area;
+        area = (Math.PI*Math.pow(radio,2));
+        return area;
+
+    }
+    public double calcularDiametro (){
+        return radio*2;
+
+
+    }
+    public double calcularCircunferencia(){
+        //opcion 1
+        //return Math.PI*radio*2;
+        //opcion 2
+        return Math.PI*calcularDiametro();
+    }
+
+    @Override
+    public String toString() {
+        return "el radio tiene un radio de: "+ radio
+                + "\n El area del ciculo es: "+calculararea()
+                +"\n El diametro del circulo es:"+ calcularDiametro()
+                +"\n La circunferencia del circulo es de: "+ calcularCircunferencia();
+
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+    Circulo objetocirculo =new Circulo(5.28);
+        objetocirculo.calcularDiametro();
+        objetocirculo.calculararea();
+        objetocirculo.calcularCircunferencia();
+        System.out.println(objetocirculo.toString());
+
+    }
 
 }
